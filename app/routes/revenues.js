ec9ef61066262a865beee938c72e85b028c64fbe
GaspardPierre <pierredillard@gmail.com>
2024-02-26 16:03:43 +0100

    add revenues/post

--- app/routes/revenues.js
+++ app/routes/revenues.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Revenue = require('../models/Revenue')
 const dbMiddleware = require('../../middleware/dbMiddleware');
 
 router.get('/', dbMiddleware, async (req, res) => {
@@ -11,6 +12,20 @@ router.get('/', dbMiddleware, async (req, res) => {
   } catch (error) {
     console.error('Database Error:', error);
     res.status(500).send('Internal Server Error');
+  }
+});
+
+
+router.post('/addRevenue', async (req, res) => {
+  try {
+    const newRevenue = new Revenue({
+      month: req.body.month,
+      revenue: req.body.revenue
+    });
+    const result = await newRevenue.save();
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).send('Erreur de serveur: ' + error.message);
   }
 });
 
